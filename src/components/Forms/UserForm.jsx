d785ab8ecf14dd2081c98260c87e9701efe76484
Vit Benton <Vit@vitbenton.com>
2025-05-06 09:21:17 -0400

    update email functionality

--- src/components/Forms/UserForm.jsx
+++ src/components/Forms/UserForm.jsx
@@ -1,11 +1,37 @@
+import { useState } from 'react'
+import { useNotification } from '../../contexts/NotificationContext'
 import { useSession } from '../../contexts/SessionContext'
 
 export default function UserForm() {
-  const { userEmail } = useSession()
+  const { updateEmail, userEmail } = useSession()
+  const [newEmail, setEmail] = useState('')
+
+  const { notify } = useNotification()
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+    try {
+      await updateEmail(newEmail)
+      notify('✅ Email updated successfully')
+    } catch (err) {
+      notify(`❌ Failed to update email: ${err.message}`)
+    }
+  }
 
   return (
-    <>
-      <p>Email: {userEmail}</p>
-    </>
-  );
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input
+          type='email'
+          value={newEmail}
+          placeholder={userEmail}
+          onChange={e => setEmail(e.target.value)}
+          required
+        />
+      </label>
+      <button disabled={!newEmail} type='submit'>Update Email</button>
+    </form>
+  )
 }
